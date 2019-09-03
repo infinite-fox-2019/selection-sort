ce24632f19040f484140fd1c1f25c8637a1ad5ed
elizabethwidiasri <elizabethwidiasri@gmail.com>
2019-09-03 14:03:06 +0700

    plus release1

--- selection_sort.js
+++ selection_sort.js
@@ -1,4 +1,6 @@
-//smallest value swapped with the leftmost number in array
+
+
+//basic linear search
 function linearSearch(search, arr) {
     
     for (var i=0; i<arr.length; i++) {
@@ -11,6 +13,19 @@ function linearSearch(search, arr) {
 
 }
 let random_numbers= [6, 29, 18, 2, 72, 19, 18, 10, 37];
-console.log(linearSearch(18, random_numbers));
-console.log(linearSearch(9, random_numbers));
+// console.log(linearSearch(18, random_numbers));
+// console.log(linearSearch(9, random_numbers));
 
+//global linear search
+
+function globalLinearSearch(search, arr) {
+    var res = []
+    for (var i=0; i<arr.length; i++) {
+        if (search === arr[i]) {
+            res.push(i)
+        }
+    }
+    return res
+}
+let banana_arr = "banana".split(""); //["b", "a", "n", "a", "n", "a"]
+console.log(globalLinearSearch("a", banana_arr)); //[1, 3, 5]
